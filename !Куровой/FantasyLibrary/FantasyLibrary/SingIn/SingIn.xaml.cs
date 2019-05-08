80c5e104257c9cf6b08de450742afaed7b8065c6
xlOZlx <dima1562.17@gmail.com>
2019-05-08 14:59:47 +0300

    Add Project

---
+++ !Куровой/FantasyLibrary/FantasyLibrary/SingIn/SingIn.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FantasyLibrary
+{
+    /// <summary>
+    /// Логика взаимодействия для SingIn.xaml
+    /// </summary>
+    public partial class SingIn : Window
+    {
+        public SingIn()
+        {
+            InitializeComponent();
+            MainFrame.Content = new SingInPage();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ !Куровой/FantasyLibrary/FantasyLibrary/SingIn/SingInPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FantasyLibrary
+{
+    /// <summary>
+    /// Логика взаимодействия для SingInPage.xaml
+    /// </summary>
+    public partial class SingInPage : Page
+    {
+        public SingInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSingIn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mWin = new MainWindow();
+            SingIn wSingIn = (SingIn)App.Current.MainWindow;
+            mWin.Show();
+            wSingIn.Close();
+        }
+
+        private void ButtonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            SingInRegister singInRegister = new SingInRegister();
+            SingIn wSingIn = (SingIn)App.Current.MainWindow;
+            wSingIn.MainFrame.Content = singInRegister;
+        }
+    }
+}
